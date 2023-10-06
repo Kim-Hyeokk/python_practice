2b3ccb3b50e1be84b5000512e11ff1746f853431
Kim-Hyeokk <97571063+Kim-Hyeokk@users.noreply.github.com>
2023-10-06 22:43:52 +0900

    숫자형 및 사칙연산
    2023.10.06

---
+++ 숫자형.py
@@ -0,0 +1,30 @@
+# 정수형
+a = 123
+a = -178
+a = 0
+
+# 실수형
+a = 1.2
+a = -3.45
+a = 4.24E10
+a = 4.24e-10 # e나 E 둘 중 어느 것을 사용해도 됨
+
+#8진수와 16진수
+a = 0o177
+print(a)
+
+a = 0x8ff
+b = 0xABC
+print(b)
+
+#사칙 연산
+a = 3
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b) # x의 y제곱
+print(7%3) # 나눗셈 후 나머지
+print(7//4) # 나눗셈 후 몫
+
